c85f0212a634db4e30e13b476c081f7ccc802a2b
ray <ray@c047d178-4cc5-419b-8a40-3dd4ef740191>
2008-06-09 20:44:54 +0000

    Logging working except for POST.
    git-svn-id: file:///Users/ray/Public/svn/timelord/trunk@8 c047d178-4cc5-419b-8a40-3dd4ef740191

--- media/js/log.js
+++ media/js/log.js
@@ -1,22 +1,40 @@
 /* Script to log times to Timelord */
 /* Timer documentation - http://jquery.offput.ca/every/ */
 
-var times = new Array();
+var INTERVAL = 10;//<--------------------- Change to 60
+var INTERVAL_STR = "" + INTERVAL + "s";
+
+var times = [];
+var totalTime = 0;
 
 $(document).ready(function(){
-    $(document).everyTime("30s", "log_timer", log);
+    $(document).everyTime(INTERVAL_STR, "log_timer", log);
     $("#paused").click(clickPaused);
 })
 
 function log() {
-    var idx = $("#task").selectedIndex;
-    alert(idx);
+    var idx = $("#task").val();
+    
+    var time = times[idx];
+    if (time == undefined) {
+        times[idx] = INTERVAL;
+    } else {
+        times[idx] += INTERVAL;
+    }
+    
+    totalTime += INTERVAL;
+    // Log every five minutes
+    if (totalTime >= 30) {//<------------ Change to 300
+        alert("log");
+        totalTime = 0;
+        times = [];
+    }
 }
 
 function clickPaused() {
     if (this.checked) {
         $(document).stopTime("log_timer");
     } else {
-        $(document).everyTime("30s", "log_timer", log);
+        $(document).everyTime(INTERVAL_STR, "log_timer", log);
     }
 }
