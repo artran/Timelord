afb259b4ac05fe749ec0720a34aac1cb9cd0234d
ray <ray@c047d178-4cc5-419b-8a40-3dd4ef740191>
2008-06-11 20:33:54 +0000

    Logging now working.
    git-svn-id: file:///Users/ray/Public/svn/timelord/trunk@12 c047d178-4cc5-419b-8a40-3dd4ef740191

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
 
@@ -13,6 +14,7 @@ def project(request, id):
     project = get_object_or_404(Project, pk=id)
     return render_to_response('main/project.html', {'project': project})
 
+@login_required
 def status(request):
     user = request.user
     project = None
@@ -49,11 +51,11 @@ def status(request):
                                                    'tasks': tasks, 'milestones': milestones,
                                                    'today_time': today_time,'task_time': task_time})
 
+@login_required
 def log(request):
     if request.method != 'POST':
         return status(request)
     
-    print request.POST
     user = request.user
     for item in request.POST.items():
         key = item[0]
@@ -69,41 +71,10 @@ def log(request):
                 log.save()
             except:
                 pass
-            
-    current_task = None
-    task_time = '0:00'
-    today_time = '0:00'
-    today = date.today()
-    try:
-        current_task = Task.objects.get(pk=request.POST['task'])
-        # Get time for the current task
-        log_entries = LogEntry.objects.filter(staff=user, task=current_task,
-                      logged_at__year=today.year, logged_at__month=today.month,
-                      logged_at__day=today.day)
-        task_mins = 0
-        for entry in log_entries:
-            task_mins += entry.delta_time
-        task_hours = task_mins // 60
-        task_mins = task_mins - (60 * task_hours)
-        task_time = "%i:%#02i" % (task_hours, task_mins)
-        
-        # Get the total time for today
-        log_entries = LogEntry.objects.filter(staff=user,
-                              logged_at__year=today.year, logged_at__month=today.month,
-                              logged_at__day=today.day)
-        today_mins = 0
-        for entry in log_entries:
-            today_mins += entry.delta_time
-        today_hours = today_mins // 60
-        today_mins = today_mins - (60 * today_hours)
-        today_time = "%i:%#02i" % (today_hours, today_mins)
-    except:
-        print 'exception'
     
-    return render_to_response('main/log-result.xml', {'current_task': current_task, 'task_time': task_time,
-                                                       'today_time': today_time})
+    return task_status(request)        
 
-# This function repeats a chunk of the log function
+@login_required
 def task_status(request):
     user = request.user
     current_task = None
