c6eeca99a3d0952dde1e260bc7eb27ca141728c6
ray <ray@c047d178-4cc5-419b-8a40-3dd4ef740191>
2008-08-05 19:29:36 +0000

    Reworked notes and expenses so they have a FK to LogEntry. The LogEntry holds enough info for them to drop task and staff fields and not need a datetime adding. This completes #7.
    Changed Expenses model to use a text field for the text. This completes #8.
    
    
    git-svn-id: file:///Users/ray/Public/svn/timelord/trunk@22 c047d178-4cc5-419b-8a40-3dd4ef740191

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+# This Python file uses the following encoding: utf-8
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -44,21 +46,32 @@ class Task(models.Model):
         list_display_links = ('name',)
         ordering = ('project', 'name')
         list_filter = ('project', 'name')
+
+class LogEntry(models.Model):
+    task = models.ForeignKey(Task, related_name='log-entries')
+    staff = models.ForeignKey(User, related_name='log-entries')
+    logged_at = models.DateTimeField(default=datetime.now)
+    delta_time = models.IntegerField(help_text="Number of minutes being logged")
+    def __str__(self):
+        return "%s, %s: %s" % (self.task, self.staff, self.delta_time)
+    class Admin:
+        save_on_top = True
+        list_display = ('task', 'staff', 'delta_time', 'logged_at')
+        list_filter = ('task', 'logged_at')
+        ordering = ['-logged_at']
     
 class Note(models.Model):
-    task = models.ForeignKey(Task, related_name='notes')
-    staff = models.ForeignKey(User, related_name='notes')
-    text = models.TextField()
+    text = models.TextField(core=True)
+    log_entry = models.ForeignKey(LogEntry, edit_inline=models.TABULAR, num_in_admin=1)
     def __str__(self):
         return "%s: %s" % (self.task, self.text)
     class Admin:
         pass
     
 class Expense(models.Model):
-    task = models.ForeignKey(Task, related_name='expenses')
-    staff = models.ForeignKey(User, related_name='expenses')
-    text = models.CharField(max_length=50)
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    text = models.TextField()
+    amount = models.DecimalField(max_digits=5, decimal_places=2, help_text="£", core=True)
+    log_entry = models.ForeignKey(LogEntry, edit_inline=models.TABULAR, num_in_admin=1)
     def __str__(self):
         return "%s: %s" % (self.task, self.text)
     class Admin:
@@ -81,16 +94,5 @@ class Milestone(models.Model):
         return "%s: %s" % (self.task, self.name)
     class Meta:
         unique_together = ('name', 'task')
-    class Admin:
-        pass
-        
-class LogEntry(models.Model):
-    task = models.ForeignKey(Task, related_name='log-entries')
-    staff = models.ForeignKey(User, related_name='log-entries')
-    logged_at = models.DateTimeField(default=datetime.now)
-    delta_time = models.IntegerField(help_text="Number of minutes being logged")
-    note = models.ForeignKey(Note, blank=True, null=True)
-    def __str__(self):
-        return "%s, %s: %s" % (self.task, self.staff, self.delta_time)
     class Admin:
         pass
